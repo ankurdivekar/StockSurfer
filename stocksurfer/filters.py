95a5c623500c0a42fd1fe7925ded8326b0ebed90
Ankur Divekar <ankur.divekar@gmail.com>
2023-05-06 00:01:28 +0530

    Added wedge_200_20, n_detections

--- stocksurfer/filters.py
+++ stocksurfer/filters.py
@@ -2,8 +2,8 @@
 
 # %% auto 0
 __all__ = ['base_path', 'processed_data_dir', 'nifty500_csv', 'get_nifty500', 'get_symbol_data', 'get_monthly_data',
-           'get_weekly_data', 'filter_stocks', 'single_candle_span', 'hammer_on_BBL', 'green_engulfing_on_BBL',
-           'three_rising_green_candles_on_SMA20', 'level_catch', 'alltime_high']
+           'get_weekly_data', 'filter_stocks', 'wedge_200_20', 'single_candle_span', 'hammer_on_BBL',
+           'green_engulfing_on_BBL', 'three_rising_green_candles_on_SMA20', 'level_catch', 'alltime_high']
 
 # %% ../nbs/04_filters.ipynb 3
 import pandas as pd
@@ -82,6 +82,7 @@ def filter_stocks(
     timeframe="daily",
     cutoff_date=None,
     lookback=0,
+    n_detections=0,
     strategy=None,
     strategy_args=None,
 ):
@@ -94,6 +95,9 @@ def filter_stocks(
         df = get_symbol_data(symbol)
         if df is None:
             print(f"Data not found for {symbol}")
+        elif len(df) < 202:
+            # print(f"Data not sufficient for {symbol}: {len(df)} rows")
+            pass
         else:
             # Filter data by cutoff data
             if cutoff_date:
@@ -106,13 +110,79 @@ def filter_stocks(
                 df = add_all_technicals(get_weekly_data(df))
 
             # Iteratively evaluate strategy for lookback period
+            detection_count = 0
             for lb in range(min(lookback+1, len(df)-1)):
                 df_lb = df.drop(df.tail(lb).index)
 
                 # Pass strategy args to the strategy method and run it
-                strategy(df_lb, kwargs=strategy_args)
+                if strategy(df_lb, kwargs=strategy_args):
+                    detection_count += 1
+                    if detection_count == n_detections:
+                        break
 
-# %% ../nbs/04_filters.ipynb 12
+# %% ../nbs/04_filters.ipynb 16
+# Check for a 200-20 wedge position
+def wedge_200_20(df, kwargs=None):
+    
+    # Get args
+    window = kwargs['window'] if kwargs and 'window' in kwargs.keys() else 12
+    
+    # Get df tail
+    df_tail = df.tail(window)
+    
+    # Get 200-20 diff
+    tail_diff = df_tail.apply(lambda x: x.SMA_200_C - x.SMA_20_C, axis=1)
+    
+    conditions = [
+        
+        # SMA 20 is rising
+        df_tail.SMA_20_C.is_monotonic_increasing,
+        
+        # SMA 20 is roughly rising, calculated as 75% of candles are closing higher than previous candle
+        # ((df_tail.SMA_20_C.diff() > 0).sum()+1)/(len(df_tail)) > 0.75,
+        
+        # SMA 200 and SMA 20 are converging
+        tail_diff.is_monotonic_decreasing,
+
+        # SMA 200 is above SMA 20
+        all(tail_diff > 0),
+
+        # SMA 200 and SMA 20 are within x% of each other
+        all(tail_diff < df_tail.SMA_200_C * 0.2),
+        
+        # Any of these positions
+        any([
+            # Last candle has crossed SMA 200 with green candle
+            df.iloc[-1].CLOSE > df.iloc[-1].SMA_200_C > df.iloc[-1].LOW,
+            # Last candle is cleanly above SMA 200 and the one before spanned SMA 200 but closed below it
+            df.iloc[-1].LOW > df.iloc[-1].SMA_200_C and df.iloc[-2].HIGH > df.iloc[-2].SMA_200_C > df.iloc[-2].LOW,
+            # Last candle is cleanly above SMA 200 and the one before is cleanly below SMA 200
+            df.iloc[-1].LOW > df.iloc[-1].SMA_200_C and df.iloc[-2].HIGH < df.iloc[-2].SMA_200_C,
+        ]),
+    
+        
+        # Candle before last has closed below SMA 200
+        # df.iloc[-2].CLOSE < df.iloc[-2].SMA_200_C,
+        
+        # Body of last candle should be bigger than upper and lower wick
+        df.iloc[-1].CLOSE - df.iloc[-1].OPEN > df.iloc[-1].HIGH - df.iloc[-1].CLOSE,
+        df.iloc[-1].CLOSE - df.iloc[-1].OPEN > df.iloc[-1].OPEN - df.iloc[-1].LOW,
+        
+        # SMA 20 crosses over SMA 200 from below
+        # df.iloc[-1].SMA_20_C > df.iloc[-1].SMA_200_C,
+        # df.iloc[-2].SMA_20_C < df.iloc[-2].SMA_200_C,
+
+        # df.iloc[-2].CLOSE < df.iloc[-2].SMA_200_C,
+        # df.iloc[-3].CLOSE < df.iloc[-3].SMA_200_C,
+
+    ]
+
+    if all(conditions):
+        print(f"{df.iloc[-1].SYMBOL} is in a 200-20 wedge position {df.DATE.iloc[-1].date()} at {df.iloc[-1].CLOSE}")
+        return True
+    return False
+
+# %% ../nbs/04_filters.ipynb 19
 # Check if the latest candle spans the given SMAs
 def single_candle_span(df, kwargs=None):
     if kwargs and "col_list" in kwargs.keys():
@@ -128,7 +198,7 @@ def single_candle_span(df, kwargs=None):
         return True
     return False
 
-# %% ../nbs/04_filters.ipynb 15
+# %% ../nbs/04_filters.ipynb 22
 # Check if the latest candle is a hammer
 def hammer_on_BBL(df, kwargs=None):
     body = df.iloc[-1].CLOSE - df.iloc[-1].OPEN
@@ -147,7 +217,7 @@ def hammer_on_BBL(df, kwargs=None):
         return True
     return False
 
-# %% ../nbs/04_filters.ipynb 18
+# %% ../nbs/04_filters.ipynb 25
 # Check if latest candle is green takes out red on BBL
 def green_engulfing_on_BBL(df, kwargs=None):
     conditions = [
@@ -164,7 +234,7 @@ def green_engulfing_on_BBL(df, kwargs=None):
         return True
     return False
 
-# %% ../nbs/04_filters.ipynb 21
+# %% ../nbs/04_filters.ipynb 28
 # Check for three rising green candles
 def three_rising_green_candles_on_SMA20(df, kwargs=None):
     conditions = [
@@ -184,7 +254,7 @@ def three_rising_green_candles_on_SMA20(df, kwargs=None):
         return True
     return False
 
-# %% ../nbs/04_filters.ipynb 24
+# %% ../nbs/04_filters.ipynb 31
 # Check for SMA 20 catch
 def level_catch(df, kwargs=None):
     if kwargs and "level" in kwargs.keys():
@@ -203,7 +273,7 @@ def level_catch(df, kwargs=None):
         print("Level not specified")
     return False
 
-# %% ../nbs/04_filters.ipynb 27
+# %% ../nbs/04_filters.ipynb 34
 # Check for alltime high
 def alltime_high(df, kwargs=None):
     df2 = df[:-1]
